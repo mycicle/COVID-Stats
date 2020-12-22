3f67cb3427df3d0c9799b42a456048929c27e97e
Mycicle <mjm.digregorio@gmail.com>
2020-12-22 17:38:10 -0500

    enums: added enums for popular column values added function to return a subset of a dataset based off of a list of acceptable or unacceptable values
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- src/deaths_by_age_week.py
+++ src/deaths_by_age_week.py
@@ -10,21 +10,37 @@ import numpy as np
 from io import StringIO
 from sklearn.preprocessing import LabelEncoder
 from loguru import logger
-from typing import Dict, List, Tuple, Any
+from typing import Dict, List, Tuple, Any, Union
 
-from utils.enums import DatasetName
-from utils.variables import path_dict, age_key
+from utils.enums import DatasetName, AgeGroup, Sexes
+from utils.variables import path_dict, age_key, sex_key, week_key
 
 def to_categorical(input: pd.Series) -> List[Any]:
     """
     returns the mapping of int to categorical value, and the categorized pandas series
     """
+
     le = LabelEncoder()
     output: np.ndarray =  le.fit_transform(input)
     le_name_mapping: Tuple[int, str] = tuple(zip(le.transform(le.classes_), le.classes_))
     
     return [le_name_mapping, pd.Series(output)]
 
+def get_rows(data: pd.DataFrame, column_key: str, column_values: List[Any], includes: bool = True):
+    """
+    input a dataframe, a column key, a list of column values, and a bool for includes (true by default,
+    true to return values that match the column_values and false to return values that exclude column_values)
+
+    returns a data frame
+    """
+
+    if includes:
+        return data[data[column_key].isin(column_values)]
+    else:
+        return data[~data[column_key].isin(column_values)]
+
+
+@logger.catch
 def main():
     datapath: str = path_dict[DatasetName.sex_age_week]
     
@@ -36,10 +52,22 @@ def main():
     logger.info(f"Dataset Length: {len(raw)}")
     logger.info(f"Dataset Summary: {buf.getvalue()}")
 
-    logger.info(f"Categorizing column: {age_key}")
-    [age_categorical_mapping, raw[age_key]] = to_categorical(raw[age_key])
+    # logger.info("Generating Dataset for Ages 0-24")
+    # lets get the death rate over time for ages 0-24
+    # ages_0_24: pd.DataFrame = raw[((raw[age_key] == AgeGroup.under_one_year.value) | 
+    #                                 (raw[age_key] == AgeGroup.age_1_4.value) |
+    #                                 (raw[age_key] == AgeGroup.age_5_14.value) | 
+    #                                 (raw[age_key] == AgeGroup.age_15_24.value))
+    #                                 &
+    #                                 (raw[sex_key] == Sexes.both_sexes.value)
+    #                             ]
+    # buf: StringIO = StringIO()
+    # ages_0_24.info(verbose=True, buf=buf)
+    # logger.info(f"Dataset Summary: {buf.getvalue()}")
 
-    logger.info(f"Categorical Mappings for column: {age_key}\n{age_categorical_mapping}")
+    # min_week: int = raw[week_key].min()
+    # max_week: int = raw[week_key].max()
+
 
 
 if __name__ == "__main__":
--- src/utils/enums.py
+++ src/utils/enums.py
@@ -8,3 +8,22 @@ class DatasetName(Enum):
     conditions = "conditions"
     weekly_deaths_14_18 = "weekly_deaths_14_18"
     weekly_deaths_19_20 = "weekly_deaths_19_20"
+
+class AgeGroup(Enum):
+    all_ages = "All Ages"
+    under_one_year =  "Under 1 year"
+    age_1_4        =  "1-4 years"
+    age_5_14       =  "5- 14 years"
+    age_15_24      =  "15- 24 years"
+    age_25_34      =  "25- 34 years"
+    age_35_44      =  "35- 44 years"
+    age_45_54      =  "45- 54 years"
+    age_55_64      =  "55- 64 years"
+    age_65_74      =  "65- 74 years"
+    age_75_84      =  "75- 84 years"
+    age_85_over    =  "85 years and over"
+
+class Sexes(Enum):
+    male       = "Male"
+    female     = "Female"
+    both_sexes = "All Sex"
