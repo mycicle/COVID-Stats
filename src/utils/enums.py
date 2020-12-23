5c08d62f1e99d5ca1cfdcffd0bd988a929940920
Michael DiGregorio <mjm.digregorio@gmail.com>
2020-12-22 21:02:32 -0500

    Update enums.py
    format enums to be a bit more readable

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class DatasetName(Enum):
-    sex_age_state = "sex_age_state"
-    sex_age_week = "sex_age_week"
-    focus_ages_0_18 = "focus_ages_0_18"
-    dc_inf_pneu = "dc_inf_pneu"
-    conditions = "conditions"
+    sex_age_state       = "sex_age_state"
+    sex_age_week        = "sex_age_week"
+    focus_ages_0_18     = "focus_ages_0_18"
+    dc_inf_pneu         = "dc_inf_pneu"
+    conditions          = "conditions"
     weekly_deaths_14_18 = "weekly_deaths_14_18"
     weekly_deaths_19_20 = "weekly_deaths_19_20"
 
